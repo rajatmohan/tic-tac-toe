c1a439f77790d3889d10d655ace5f88216856c69
rajatmohan <rajatm2196@gmail.com>
2022-11-06 17:09:20 +0530

    Add player info and display game result with structural changes

--- script.js
+++ script.js
@@ -1,70 +1,78 @@
 const boardSideLength = 3;
 
-const DisplayController = (
-    (boardSideLength)=> {
-        const _board = document.querySelector("#board");
-        const _boardSideLength = boardSideLength;
+const Player = (
+    (name, sign) => {
+        let _name = name;
+        let _sign = sign;
 
-        const _startButton = document.querySelector("#startButton");
-
-        const _makeCell = (index)=> {
-            const cell = document.createElement('div');
-            cell.classList.add("board-cell")
-
-            cell.addEventListener('click', ()=>GameController.playerClick(index));
-            return cell;
-        };
-
-        const init = ()=>{
-            _board.style.gridTemplateColumns = `repeat(${_boardSideLength}, minmax(0, 1fr))`;
-            for(let i = 0; i < _boardSideLength*_boardSideLength; i++) {
-                _board.appendChild(_makeCell(i));
-            }
-            _startButton.addEventListener('click', _startButtonClicked);
-        };
-
-        const setCell = (index, sign)=> {
-            _board.querySelector(`#board > div:nth-child(${index+1})`).textContent = sign;
-        };
-
-        const reset = () => {
-            _board.querySelectorAll('#board > div').forEach((divEle)=>{
-                divEle.textContent = '';
-            });
-        }
-
-        const _startButtonClicked = (event)=> {
-            event.preventDefault();
-            if(event.target.textContent == "Start Game") {
-                event.target.textContent = 'Restart Game';
-                GameController.startButtonClicked();
-            }
-            else {
-                event.target.textContent = 'Restart Game';
-                GameController.restartButtonClicked();
-            }
-        }
+        const getName = () => _name;
+        const setName = (name) => _name = name;
+        const getSign = () => _sign;
+        const setSign = (sign) => _sign = sign;
 
         return {
-            init,
-            setCell,
+            getName,
+            setName,
+            getSign,
+            setSign,
+        }
+    }
+);
+
+const GameBoard = (
+    (boardSideLength) => {
+        const _boardSize = boardSideLength*boardSideLength;
+        const _board = [...Array(_boardSize).fill('')];
+
+        const getBoard = () => _board;
+
+        const reset = ()=> {
+            for(let i = 0; i < _boardSize; i++) {
+                _board[i] = '';
+            }
+        };
+
+        const getCell = (index) => {
+            return _board[index];
+        }
+
+        const setCell = (index, sign)=> {
+            if(_board[index] === '') {
+                _board[index] = sign;
+                return true;
+            }
+            return false;
+        };
+
+        const getEmptyCell = () => {
+            const fields = [];
+            for(let i = 0; i < _boardSize; i++) {
+                if(_board[i] === '') {
+                    fields.push(i);
+                }
+            }
+            return fields;
+        };
+
+        return {
+            getBoard,
             reset,
+            getCell,
+            setCell,
+            getEmptyCell,
         };
     }
 )(boardSideLength);
 
 const GameController = ((boardSideLength)=> {
-    let _sign = 'X';
     const _boardSideLength = boardSideLength;
     let _state = 'GAME_NOT_STARTED';
 
+    let _playerList = [Player('Player 1', 'X'), Player('Player 2', 'O')];
+    let _currentPlayer = _playerList[0];
+
     const _toggleTurn = ()=> {
-        if(_sign == 'X') {
-            _sign = 'O';
-        }
-        else {
-            _sign = 'X';
-        }
+        _currentPlayer = (_currentPlayer === _playerList[0])? _playerList[1]: _playerList[0];
     };
 
     const _checkWinConditionRow = (index) => {
@@ -136,79 +144,191 @@ const GameController = ((boardSideLength)=> {
     };
 
     const playerClick = (index) => {
-        if(this._state === "GAME_STARTED" && GameBoard.setCell(index, _sign)) {
-            _toggleTurn();
-            console.log(_checkGameWinCondition(index));
-            console.log(_checkDrawCondition(index));
+        if(_state === "GAME_STARTED" && GameBoard.setCell(index, _currentPlayer.getSign())) {
+            if(_checkGameWinCondition(index)) {
+                _state = "GAME_WON";
+            }
+            else if(_checkDrawCondition(index)) {
+                _state = "GAME_DRAW";
+            }
+            else {
+                _toggleTurn();
+            }
+            return true;
         }
+        return false;
+    };
+    
+    const getAllPlayers = ()=> {
+        return _playerList;
+    };
+
+    const getCurrentPlayer = ()=> {
+        return _currentPlayer;
     };
 
     const startButtonClicked = ()=> {
-        this._state = "GAME_STARTED";
+        _state = "GAME_STARTED";
     };
 
     const restartButtonClicked = ()=> {
-        this._state = "GAME_STARTED";
+        _state = "GAME_STARTED";
+        _currentPlayer = _playerList[0];
         GameBoard.reset();
     }
 
-    const init = ()=> {
-        DisplayController.init();
+    const isGameWon = ()=> {
+        return _state === "GAME_WON"? true: false;
+    }
+
+    const isGameDraw = ()=> {
+        return _state === "GAME_DRAW"? true: false;
     }
 
     return {
-        init,
+        getAllPlayers,
+        getCurrentPlayer,
         playerClick,
         startButtonClicked,
         restartButtonClicked,
+        isGameDraw,
+        isGameWon,
     };
     
 })(boardSideLength);
 
-const GameBoard = (
-    (boardSideLength) => {
-        const _boardSize = boardSideLength*boardSideLength;
-        const _board = [...Array(_boardSize).fill('')];
+const DisplayController = (
+    (boardSideLength)=> {
+        const _board = document.querySelector("#board");
+        const _playerInfo = document.querySelector("#playerInfoContainer");
+        const _boardSideLength = boardSideLength;
+        const _startButton = document.querySelector("#startButton");
+        const _gameEndMessageDiv = document.querySelector("#gameEndMessageModal");
+        const _gameEndMessage = document.querySelector("#gameEndMessage");
+        const __gameEndMessageCloseBtn = _gameEndMessageDiv.querySelector(".close");
 
-        const reset = ()=> {
-            for(let i = 0; i < _boardSize; i++) {
-                _board[i] = '';
-            }
-            DisplayController.reset();
-        };
-
-        const getCell = (index) => {
-            return _board[index];
+        // Game end message modal close button functionality
+        __gameEndMessageCloseBtn.onclick = () => {
+            _gameEndMessageDiv.style.display = "none";
         }
 
-        const setCell = (index, sign)=> {
-            if(_board[index] === '') {
-                _board[index] = sign;
-                DisplayController.setCell(index, sign);
-                return true;
-            }
-            return false;
+        const _hightLightCurrentPlayer = ()=>{
+            let currentPlayer = GameController.getCurrentPlayer()
+            Array.from(_playerInfo.children).forEach(child => {
+                console.log(child.dataset.sign + " " +currentPlayer.getSign())
+                if(child.dataset.sign === currentPlayer.getSign()) {
+                    child.classList.add("player-active");
+                }
+                else {
+                    child.classList.remove("player-active");
+                }
+            });
         };
 
-        const getEmptyCell = () => {
-            const fields = [];
-            for(let i = 0; i < _boardSize; i++) {
-                if(_board[i] === '') {
-                    fields.push(i);
+        const _winnerDisplay = (player)=> {
+            _gameEndMessageDiv.style.display = "block";
+            _gameEndMessage.textContent = `${player.getName()} Won !`;
+        }
+
+        const _drawDisplay = ()=> {
+            _gameEndMessageDiv.style.display = "block";
+            _gameEndMessage.textContent = `Draw`;
+        }
+
+
+        const _playerClick = (e)=> {
+            let index = parseInt(e.target.dataset.index)
+            if(index >= 0) {
+                let sign = GameController.getCurrentPlayer().getSign();
+                if(GameController.playerClick(index)) {
+                    _setCell(index, sign);
+
+                    // win draw else toggle
+                    if(GameController.isGameWon()) {
+                        _winnerDisplay(GameController.getCurrentPlayer());
+                    }
+                    else if(GameController.isGameDraw()) {
+                        _drawDisplay();
+                    }
+                    else {
+                        _hightLightCurrentPlayer();
+                    }
                 }
             }
-            return fields;
+        };
+
+        const _makeCell = (index)=> {
+            const cell = document.createElement('div');
+            cell.classList.add("board-cell")
+            cell.dataset.index = index;
+            cell.addEventListener('click', _playerClick);
+            return cell;
+        };
+
+        const _editPlayerName = (e)=> {
+            let playerEdited = GameController.getAllPlayers().filter(player => player.getSign() === e.target.dataset.sign);    
+            if(playerEdited.length == 1) {
+                playerEdited[0].setName(e.target.textContent);
+            }
+        }
+
+        const _makePlayerInfo = (player)=> {
+            const playerDiv = document.createElement('div');
+            playerDiv.setAttribute("contentEditable", "true");
+            playerDiv.classList.add("player-info")
+            playerDiv.dataset.sign = player.getSign();
+            playerDiv.textContent = `${player.getName()}`;
+            playerDiv.onkeyup = _editPlayerName;
+            return playerDiv;
+        }
+
+        const _startButtonClicked = (event)=> {
+            event.preventDefault();
+            if(event.target.textContent == "Start Game") {
+                event.target.textContent = 'Restart Game';
+                GameController.startButtonClicked();
+            }
+            else {
+                event.target.textContent = 'Restart Game';
+                GameController.restartButtonClicked();
+                _reset();
+            }
+
+            _hightLightCurrentPlayer();
+        }
+
+        const _setCell = (index, sign)=> {
+            _board.querySelector(`#board > div:nth-child(${index+1})`).textContent = sign;
+        };
+
+        const _reset = () => {
+            _board.querySelectorAll('#board > div').forEach((divEle)=>{
+                divEle.textContent = '';
+            });
+            GameController.restartButtonClicked();
+        }
+
+        const init = ()=>{
+            // create board
+            _board.style.gridTemplateColumns = `repeat(${_boardSideLength}, minmax(0, 1fr))`;
+            for(let i = 0; i < _boardSideLength*_boardSideLength; i++) {
+                _board.appendChild(_makeCell(i));
+            }
+            _startButton.addEventListener('click', _startButtonClicked);
+
+            // create player info
+            GameController.getAllPlayers().forEach(player => {
+                _playerInfo.appendChild(_makePlayerInfo(player));
+            });
+
         };
 
         return {
-            reset,
-            getCell,
-            setCell,
-            getEmptyCell,
+            init
         };
     }
 )(boardSideLength);
 
-GameController.init();
+DisplayController.init();
 
 
